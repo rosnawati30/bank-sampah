df9c3ef4cc20cc6b3ec9b95bf3ca8b9bdff867b0
fitristachan <120694803+fitristachan@users.noreply.github.com>
2024-05-24 19:56:41 +0700

    change color fix icon

--- app/Views/sampah/sampah_show.php
+++ app/Views/sampah/sampah_show.php
@@ -6,7 +6,7 @@
 
     <div class="w-full flex items-center justify-between">
         <div class="flex-none">
-            <button type="button" class="text-white bg-indigo-900 hover:bg-indigo-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-4 focus:outline-none">
+            <button type="button" class="text-white bg-green-800 hover:bg-indigo-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-4 focus:outline-none">
             <a href="sampah_add">
                 Tambah Data Sampah
             </a>  
--- app/Views/template/sidebar.php
+++ app/Views/template/sidebar.php
@@ -10,14 +10,14 @@
                     </svg>
                 </button>
                 <!-- <img src="/img/logosvg.svg" class="h-8 mr-3" alt="Logo" /> -->
-                <span class="self-center text-xl font-bold text-indigo-900 sm:text-2xl whitespace-nowrap"> Bank Sampah E-cogreen </span>
+                <span class="self-center text-xl font-bold text-green-800 sm:text-2xl whitespace-nowrap"> Bank Sampah E-cogreen </span>
                 </a>
             </div>
 
             <div class="flex items-center">
-                <span class="flex items-center align-middle font-semibold text-m m-1"> Admin </span>
-                <div class="flex items-center ml-3">
-                    <img class="w-8 h-8 rounded-full" src="/image/profilesvg.svg" alt="user photo">
+                <span class="flex items-center align-middle font-semibold text-green-800 text-m m-1 "> Admin </span>
+                <div class="w-8 h-8 flex items-center justify-center rounded-full bg-green-800 ">
+                  <i class="fa fa-user" style='color:white' aria-hidden="true"></i>
                 </div>
             </div>
 
@@ -29,19 +29,19 @@
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
       <ul class="space-y-2 font-medium">
         <li>
-            <a href="/" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/') ? 'bg-indigo-900 text-white hover:bg-indigo-900' : ''; ?>">
+            <a href="/" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/') ? 'bg-green-800 text-white hover:bg-green-800' : ''; ?>">
                <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 <?= url_is('/') ? 'text-white' : ''; ?>" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
                <span class="ml-3">Dashboard</span>
             </a>
          </li>
          <li>
-            <a href="/nasabah" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/nasabah') ? 'bg-indigo-900 text-white hover:bg-indigo-900' : ''; ?>">
+            <a href="/nasabah" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/nasabah') ? 'bg-green-800 text-white hover:bg-green-800' : ''; ?>">
                <svg aria-hidden="true" stroke-width="1.5" class="flex-shrink-0 size-5 text-gray-500 transition duration-75 group-hover:text-gray-900 <?= url_is('/nasabah') ? 'text-white' : ''; ?>" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" /></svg>
                <span class="ml-3"> Nasabah </span>
             </a>
          </li>
          <li>
-            <a href="/sampah" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/sampah') ? 'bg-indigo-900 text-white hover:bg-indigo-900' : ''; ?>">
+            <a href="/sampah" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 <?= url_is('/sampah') ? 'bg-green-800 text-white hover:bg-green-800' : ''; ?>">
                <svg aria-hidden="true" class="flex-shrink-0 size-6 text-gray-500 transition duration-75 group-hover:text-gray-900 <?= url_is('/sampah') ? 'text-white' : ''; ?>" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
                <span class="flex-1 ml-3 whitespace-nowrap"> Sampah </span>
             </a>
