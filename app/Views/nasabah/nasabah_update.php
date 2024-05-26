00de6d04ff37d202babeb8e8c8f344e770a238dc
rosnawati30 <rosnawati1412@gmail.com>
2024-05-26 20:10:00 +0700

    add button batal

--- app/Views/nasabah/nasabah_update.php
+++ app/Views/nasabah/nasabah_update.php
@@ -36,7 +36,7 @@
                     </div>
                 </div>
                 <div class="mt-6 flex items-center justify-center gap-x-6">
-                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Batal</button>
+                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900"><a href="<?= base_url('nasabah') ?>">Batal</a></button>
                     <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Simpan</button>
                 </div>
         </div>
