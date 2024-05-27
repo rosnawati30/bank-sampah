13c6c14775f77a8eba9c277b553e5f7f2e2be7ac
shierraanggari <shierraanggari@gmail.com>
2024-05-27 11:50:09 +0700

    added dashboard

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,43 @@
 
 namespace App\Controllers;
 
+use App\Models\TransaksiModel;
+use App\Models\NasabahModel;
+use App\Models\SampahModel;
+
 class Home extends BaseController
 {
+    protected $transaksiModel, $nasabahModel, $sampahModel;
+
+    public function __construct(){
+        $this->transaksiModel = new TransaksiModel();
+        $this->nasabahModel = new NasabahModel();
+        $this->sampahModel = new SampahModel();
+    }
+
     public function index(): string
     {
-        return view('dashboard');
+        $transaksi = $this->transaksiModel->findAll();
+
+        $totalPenjualan = 0;
+        $totalBerat = 0;
+
+        foreach ($transaksi as $t){
+            $totalPenjualan = $totalPenjualan + $t['total_harga'];
+        }
+
+        foreach ($transaksi as $t){
+            $totalBerat = $totalBerat + $t['berat'];
+        }
+
+        $data = [
+            'transaksi' => $transaksi,
+            'nasabah'=> $this->nasabahModel->findAll(),
+            'sampah' => $this->sampahModel->findAll(),
+            'totalpenjualan' => $totalPenjualan,
+            'totalberat' => $totalBerat
+        ];
+
+        return view('dashboard', $data);
     }
 }
